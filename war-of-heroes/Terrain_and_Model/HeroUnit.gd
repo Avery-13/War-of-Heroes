9023cd3d3e1d192032d675670a0d74d794bdd87c
Tommy-mk-lin <tommy.mk.lin@gmail.com>
2025-03-30 19:22:44 -0400

    Fixed Fire Animation and start of pathfinding

--- war-of-heroes/Terrain_and_Model/HeroUnit.gd
+++ war-of-heroes/Terrain_and_Model/HeroUnit.gd
@@ -11,6 +11,12 @@ var target_enemy_factory: Node3D = null  # Reference to the target enemy factory
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var animation_player : AnimationPlayer = $AnimationUnit
 
+
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+var movement_speed: float = 2.0
+var movement_target_position: Vector3 = Vector3(-3.0,0.0,2.0)
+
+
 func select() -> void:
 	is_selected = true
 	selection_indicator.visible = true  # Show the selection indicator
@@ -51,25 +57,53 @@ func convert_factory(factory: Node3D) -> void:
 		target_factory = factory
 		move_to(factory.global_transform.origin)  # Move to factory first
 
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
+	
+	# Make sure to not await during _ready.
+	actor_setup.call_deferred()
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+	var hq_building_pos = get_node("/root/Node3D/StaticBody3D_HQ")
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(hq_building_pos.position)
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
+
 func _physics_process(delta: float) -> void:
-	if target_position != Vector3.ZERO:
-		var direction = (target_position - global_transform.origin).normalized()
-		velocity = direction * speed
-		update_animation_parameters("move") # play move animation
-		look_at(global_transform.origin - direction, Vector3.UP)
-		move_and_slide()
-		
-		# Check if reached target factory
-		if target_factory and global_transform.origin.distance_to(target_position) < 3.0:
-			if global_transform.origin.distance_to(target_factory.global_transform.origin) <= 3.0:
-				_complete_conversion()
-			target_position = Vector3.ZERO
-		
-		if is_instance_valid(target_enemy_factory):
-			if global_transform.origin.distance_to(target_position) < 1.5:
-				if global_transform.origin.distance_to(target_enemy_factory.global_transform.origin) <= attack_range:
-					_convert_enemy_factory()
-				target_position = Vector3.ZERO
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector3 = global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
+	#if target_position != Vector3.ZERO:
+		#var direction = (target_position - global_transform.origin).normalized()
+		#velocity = direction * speed
+		#update_animation_parameters("move") # play move animation
+		#look_at(global_transform.origin - direction, Vector3.UP)
+		#move_and_slide()
+		#
+		## Check if reached target factory
+		#if target_factory and global_transform.origin.distance_to(target_position) < 3.0:
+			#if global_transform.origin.distance_to(target_factory.global_transform.origin) <= 3.0:
+				#_complete_conversion()
+			#target_position = Vector3.ZERO
+		#
+		#if is_instance_valid(target_enemy_factory):
+			#if global_transform.origin.distance_to(target_position) < 1.5:
+				#if global_transform.origin.distance_to(target_enemy_factory.global_transform.origin) <= attack_range:
+					#_convert_enemy_factory()
+				#target_position = Vector3.ZERO
 
 func _complete_conversion():
 	if is_instance_valid(target_factory) and target_factory.has_method("capture"):
@@ -106,9 +140,10 @@ func update_animation_parameters(action: String):
 		animation_tree["parameters/conditions/is_moving"] = false
 		animation_player.get_animation("Running_With_Gun").loop_mode = Animation.LOOP_NONE
 		animation_tree["parameters/conditions/is_firing"] = true
+		animation_tree["parameters/conditions/stop_fire"] = true
 		animation_tree["parameters/conditions/is_firing"] = false
+		animation_tree["parameters/conditions/stop_fire"] = false
 		animation_tree["parameters/conditions/is_idle"] = true
-		animation_player.get_animation("Idle_Gun_Aiming").loop_mode = Animation.LOOP_LINEAR
 	# default
 	else:
 		animation_tree["parameters/conditions/is_idle"] = true
