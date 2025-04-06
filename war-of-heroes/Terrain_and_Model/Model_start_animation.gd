21fc4fb08397ca55df00625948bb651b1ea84e66
Tommy-mk-lin <71536256+Tommy-mk-lin@users.noreply.github.com>
2025-04-06 15:44:01 -0400

    Marksman Animation

---
+++ war-of-heroes/Terrain_and_Model/Model_start_animation.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+# Reference to the AnimationPlayer
+@onready var animation_player : AnimationPlayer
+
+func _ready():
+	# Get the AnimationPlayer node
+	animation_player = $AnimationPlayer
+	# Default into idleanimation
+	var animation: Animation = animation_player.get_animation("Idle_Gun_Aiming")
+	animation.loop_mode = Animation.LOOP_LINEAR
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+		
+	
